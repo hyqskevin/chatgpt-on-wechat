d58c0267dd0fbf71c30d0e5874ae1950f20ca0c1
hanamaki <hyqskevin@gmail.com>
2025-02-14 20:13:48 +0800

    update master

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t hyqskevin/chatgpt-on-wechat .
+             -t zhayujie/chatgpt-on-wechat .
 
-docker tag hyqskevin/chatgpt-on-wechat hyqskevin/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
